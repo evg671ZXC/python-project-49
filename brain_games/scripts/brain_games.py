60728c71f0f080829d378fc814603d35787127cb
evg671ZXC <evgmine671@gmail.com>
2024-01-06 21:55:46 +0600

    4 step complited(flake8)

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,11 +3,16 @@ from brain_games import cli
 
 
 def greet():
+
     print('Welcome to the Brain Games!')
 
+
 def main():
+
     greet()
     cli.welcome_user()
 
+
 if __name__ == "__main__":
+
     main()

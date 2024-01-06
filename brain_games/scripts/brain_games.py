ff52bdf4c27a6a253b87174c58340776ce7b0613
evg671ZXC <evgmine671@gmail.com>
2024-01-06 19:57:12 +0600

    3 step complited

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+from brain_games import cli
+
+
 def greet():
     print('Welcome to the Brain Games!')
 
 def main():
     greet()
+    cli.welcome_user()
 
 if __name__ == "__main__":
     main()
